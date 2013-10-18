80b9c735b5cb83d7b7eaae04a7462d66bec92726
local-magnustools <local-magnustools@tools.wmflabs.org>
2013-10-18 15:14:49 +0000

    new toys

---
+++ public_html/flickrfree.php
@@ -0,0 +1,69 @@
+<?PHP
+
+include_once ( "php/common.php" ) ;
+
+$fk = trim ( file_get_contents ( "../flickr_key.txt" ) ) ;
+$url = "http://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=$fk&extras=license,date_upload,owner_name,icon_server,geo,tags&per_page=500" ;
+$data = file_get_contents ( $url ) ;
+$lines = explode ( "/>" , $data ) ;
+
+print get_common_header ( "flickrfree.php" , 'FlickrFree' ) ;
+print "<p>Recent free images uploaded on flickr</p>" ;
+print "<table class='table table-condensed table-striped'>" ;
+
+$licenses = array () ;
+$licenses[4] = 'CC-BY 2.0' ;
+$licenses[5] = 'CC-BY-SA 2.0' ;
+$licenses[7] = 'Public domain' ;
+
+$tdtop = "style='border-top:2px solid black'" ;
+$top = '' ;
+foreach ( $lines AS $l ) {
+	$matches = array () ;
+	preg_match ( '/license="(.)"/' , $l , $matches ) ;
+	$license = $matches[1] ;
+	if ( $license != 4 /*CC-BY*/ and $license != 5 /*CC-BY-SA*/ and $license != 7 /*PD*/ ) continue ;
+	
+	preg_match ( '/farm="([^"]+)"/' , $l , $matches ) ; $farm = $matches[1] ;
+	preg_match ( '/server="([^"]+)"/' , $l , $matches ) ; $server = $matches[1] ;
+	preg_match ( '/title="([^"]+)"/' , $l , $matches ) ; $title = $matches[1] ;
+	preg_match ( '/owner="([^"]+)"/' , $l , $matches ) ; $owner = $matches[1] ;
+	preg_match ( '/ownername="([^"]+)"/' , $l , $matches ) ; $ownername = $matches[1] ;
+	preg_match ( '/tags="([^"]+)"/' , $l , $matches ) ; $tags = $matches[1] ;
+	preg_match ( '/id="([^"]+)"/' , $l , $matches ) ; $id = $matches[1] ;
+
+	# Get thumbnail
+	$url = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=$fk&photo_id=$id" ;
+	$data = file_get_contents ( $url ) ;
+	$data = array_pop ( explode ( 'width="100"' , $data , 2 ) ) ;
+	$data = array_shift ( explode ( '/>' , $data , 2 ) ) ;
+	preg_match ( '/source="([^"]+)"/' , $data , $matches ) ; $thumb_url = $matches[1] ;
+	
+	$flickr_owner_url = "http://flickr.com/photos/$owner" ;
+	$flickr_url = "$flickr_owner_url/$id" ;
+	
+	$nn = $id ;
+	if ( $title != '' ) $nn = $title ;
+	$nn = urlencode ( "$nn.jpg" ) ;
+	$f2c = "//tools.wmflabs.org/flickr2commons/?photoid=$id" ;
+	
+	print "<tr>" ;
+	print "<td $top><a href='$flickr_url' target='_blank'><img border='0' src='$thumb_url' /></a></td>" ;
+	print "<td $top>" ;
+	print "<b>$title</b><br/>" ;
+	print "By user <i><a target='_blank' href='$flickr_owner_url'>$ownername</a></i><br/>" ;
+	print "Tags : <i>$tags</i><br/>" ;
+	print "License : <i>" . $licenses[$license] . "</i>" ;
+	print "</td><td $top><a target='_blank' href='$f2c'>Upload to Commons</a>" ;
+	print "</td>" ;
+	print "</tr>" ;
+	myflush() ;
+	
+	$top = $tdtop ;
+#	print htmlentities ( $l ) . "<br/>" ;
+}
+
+print "</table>" ;
+print get_common_footer() ;
+
+?>
---
+++ public_html/tab2wiki.php
@@ -0,0 +1,63 @@
+<?PHP
+
+include_once ( 'php/common.php' ) ;
+
+print "<html><head>" ;
+print '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' ;
+print "</head><body>" ;
+print get_common_header ( 'tab2wiki.php' , 'tab2wiki' ) ;
+
+print "<form method='post' class='form form-inline'>" ;
+if ( isset ( $_REQUEST['doit'] ) ) {
+	$compressed = isset ( $_REQUEST['compressed'] ) ;
+	$colh = isset ( $_REQUEST['colhead'] ) ? 1 : 0 ;
+	$rowh = isset ( $_REQUEST['rowhead'] ) ? 1 : 0 ;
+	if ( $rowh ) $compressed = 0 ;
+	if ( $compressed ) $sep = "||" ;
+	else $sep = "\n|" ;
+	$text = trim ( get_request ( "text" , "" ) , "\n" ) ;
+	$text = str_replace ( "\n" , "\n|-\n|" , $text ) ;
+	$text = str_replace ( "\t" , $sep , $text ) ;
+	$head = "{| border=\"1\"" ;
+	if ( isset ( $_REQUEST['sortable'] ) ) $head .= ' class="sortable"' ;
+	$text = $head . "\n|" . $text . "\n|}" ;
+	
+	if ( $colh ) {
+		$t = explode ( "\n|-\n" , $text , 2 ) ;
+		$text = array_shift ( $t ) ;
+		$text = str_replace ( "\n|" , "\n!" , $text ) ;
+		if ( $compressed ) $text = str_replace ( '||' , '!!' , $text ) ;
+		else $text = str_replace ( "\n|" , "\n!" , $text ) ;
+		$text .= "\n|-\n" . array_pop ( $t ) ;
+	}
+	
+	if ( $rowh ) {
+		$text = str_replace ( "\n|-\n|" , "\n|-\n!" , $text ) ;
+		if ( !$colh ) {
+			$t = explode ( "\n|" , $text , 2 ) ;
+			$text = array_shift ( $t ) ;
+			$text .= "\n!" . array_shift ( $t ) ;
+		}
+	}
+	
+	print "Copy the following into the wiki - done!" ;
+	print "<textarea name='text' rows='20' cols='80' style='width:100%'>" ;
+	print $text ;
+	print "</textarea>" ;
+} else {
+	print "Copy your (Excel) table, paste it here, click button, magic happens!" ;
+	print "<textarea name='text' rows='20' cols='80' style='width:100%'>" ;
+	print "</textarea>" ;
+	print "<input type='checkbox' name='colhead' value='1' checked /> First element in a column is a header | " ;
+	print "<input type='checkbox' name='rowhead' value='1' /> First element in a row is a header | " ;
+	print "<input type='checkbox' name='compressed' value='1' checked /> Compress table | " ;
+	print "<input type='checkbox' name='sortable' value='1' checked /> Sortable table<br/>" ;
+	print "<input class='btn btn-primary' type='submit' name='doit' value='Do it!' />" ;
+}
+print "</form>" ;
+
+
+
+print "</body></html>" ;
+
+?>
