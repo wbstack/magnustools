704e4cdb51d87c51f21d65f23b1aee929f874932
Magnus Manske <magnusmanske@gmail.com>
2018-04-05 10:44:29 +0000

    misc

--- public_html/resources/vue/shared.js
+++ public_html/resources/vue/shared.js
@@ -1,17 +1,24 @@
 // ENFORCE HTTPS
 if (location.protocol != 'https:') location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 
+var vue_module_templates_id = 'vue_module_templates' ;
+var vue_modules_loaded = {} ;
+
+function loadVueModule ( module , callback ) {
+	if ( typeof vue_modules_loaded[module] != 'undefined' ) return callback() ;
+	vue_modules_loaded[module] = true ;
+	var id = vue_module_templates_id + '-' + module ;
+	$('body').append($("<div id='"+id+"'>"));
+	var url = 'https://tools-static.wmflabs.org/magnustools/resources/vue/'+module+'.html' ;
+	$('#'+id).load(url,function(){ callback() }) ;
+}
+
 function loadVueModules ( modules , callback ) {
 	var cnt = 0 ;
-	var vue_module_templates_id = 'vue_module_templates' ;
-	$('body').append($("<div id='"+vue_module_templates_id+"'>"));
 	$.each ( modules , function ( dummy , module ) {
 		cnt++ ;
-		var url = 'https://tools-static.wmflabs.org/magnustools/resources/vue/'+module+'.html' ;
-		$('#'+vue_module_templates_id).append($("<div>")).load(url,function(){
-			cnt-- ;
-			if ( cnt > 0 ) return ;
-			callback() ;
-		}) ;
+		loadVueModule ( module , function(){
+			if ( --cnt == 0 ) callback() ;
+		} ) ;
 	} ) ;
 }
