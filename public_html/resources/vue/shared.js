6637a82d38d3c870ac5b9b244c0eab50f220050a
Magnus Manske <magnusmanske@gmail.com>
2018-04-05 14:31:22 +0000

    misc

--- public_html/resources/vue/shared.js
+++ public_html/resources/vue/shared.js
@@ -1,25 +1,25 @@
 // ENFORCE HTTPS
 if (location.protocol != 'https:') location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 
-var vue_modules = {
-	template_container_base_id : 'vue_module_templates' ,
-	modules_base_url : 'https://tools-static.wmflabs.org/magnustools/resources/vue/' ,
-	loadModules : function ( modules , callback ) {
+var vue_components = {
+	template_container_base_id : 'vue_component_templates' ,
+	components_base_url : 'https://tools-static.wmflabs.org/magnustools/resources/vue/' ,
+	loadComponents : function ( components , callback ) {
 		var me = this ;
 		var cnt = 0 ;
-		$.each ( modules , function ( dummy , module ) {
+		$.each ( components , function ( dummy , component ) {
 			cnt++ ;
-			me.loadModule ( module , function(){
+			me.loadComponent ( component , function(){
 				if ( --cnt == 0 ) callback() ;
 			} ) ;
 		} ) ;
 	} ,
-	loadModule ( module , callback ) {
+	loadComponent ( component , callback ) {
 		var me = this ;
-		var id = me.template_container_base_id + '-' + module ;
+		var id = me.template_container_base_id + '-' + component ;
 		if ( $('#'+id).length > 0 ) return callback() ;
-		$('body').append($("<div id='"+id+"'>"));
-		var url = me.modules_base_url+module+'.html' ;
+		$('body').append($("<div id='"+id+"' style='display:none'>"));
+		var url = me.components_base_url+component+'.html' ;
 		$('#'+id).load(url,function(){ callback() }) ;
 	}
 }
