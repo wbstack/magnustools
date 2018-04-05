b0b53dd5338495ac9c5891ffd01efb015ccb01a0
Magnus Manske <magnusmanske@gmail.com>
2018-04-05 11:19:38 +0000

    misc

--- public_html/resources/vue/shared.js
+++ public_html/resources/vue/shared.js
@@ -1,24 +1,25 @@
 // ENFORCE HTTPS
 if (location.protocol != 'https:') location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 
-var vue_module_templates_id = 'vue_module_templates' ;
-var vue_modules_loaded = {} ;
-
-function loadVueModule ( module , callback ) {
-	if ( typeof vue_modules_loaded[module] != 'undefined' ) return callback() ;
-	vue_modules_loaded[module] = true ;
-	var id = vue_module_templates_id + '-' + module ;
-	$('body').append($("<div id='"+id+"'>"));
-	var url = 'https://tools-static.wmflabs.org/magnustools/resources/vue/'+module+'.html' ;
-	$('#'+id).load(url,function(){ callback() }) ;
-}
-
-function loadVueModules ( modules , callback ) {
-	var cnt = 0 ;
-	$.each ( modules , function ( dummy , module ) {
-		cnt++ ;
-		loadVueModule ( module , function(){
-			if ( --cnt == 0 ) callback() ;
+var vue_modules = {
+	template_container_base_id : 'vue_module_templates' ,
+	modules_base_url : 'https://tools-static.wmflabs.org/magnustools/resources/vue/' ,
+	loadModules : function ( modules , callback ) {
+		var me = this ;
+		var cnt = 0 ;
+		$.each ( modules , function ( dummy , module ) {
+			cnt++ ;
+			me.loadModule ( module , function(){
+				if ( --cnt == 0 ) callback() ;
+			} ) ;
 		} ) ;
-	} ) ;
+	} ,
+	loadModule ( module , callback ) {
+		var me = this ;
+		var id = me.template_container_base_id + '-' + module ;
+		if ( $('#'+id).length > 0 ) return callback() ;
+		$('body').append($("<div id='"+id+"'>"));
+		var url = me.modules_base_url+module+'.html' ;
+		$('#'+id).load(url,function(){ callback() }) ;
+	}
 }
