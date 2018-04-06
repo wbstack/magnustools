aaebb43b631dc5401e9c1c10abe80b9359796f11
Magnus Manske <magnusmanske@gmail.com>
2018-04-06 12:00:07 +0000

    misc

--- public_html/resources/vue/shared.js
+++ public_html/resources/vue/shared.js
@@ -7,7 +7,7 @@ let vue_components = {
 	toolname : window.location.pathname.replace(/(\/|\.php|\.html{0,1})+$/,'').replace(/^.*\//,'') , // Guessing tool name, override if necessary!
 	components : {} ,
 	template_container_base_id : 'vue_component_templates' ,
-	components_base_url : 'https://tools-static.wmflabs.org/magnustools/resources/vue/' ,
+	components_base_url : 'https://tools.wmflabs.org/magnustools/resources/vue/' , // -static
 	loadComponents : function ( components ) {
 		return Promise.all ( components.map ( component => this.loadComponent(component) ) ) ;
 	} ,
@@ -21,7 +21,7 @@ let vue_components = {
 	} ,
 	loadComponent ( component ) {
 		let id = this.getComponentID ( component ) ;
-		if ( $('#'+id).length > 0 ) return Promise.resolve(42) ; // Already loaded/loading
+		if ( $('#'+id).length > 0 ) return Promise.resolve() ; // Already loaded/loading
 		$('body').append($("<div>").attr({id:id}).css({display:'none'}));
 		return fetch ( this.getComponentURL(component) )
 			.then ( (response) => response.text() )
