b446bc5e3dd4832877be00d7d6a047dcec614901
Magnus Manske <magnusmanske@gmail.com>
2018-10-17 15:03:15 +0000

    refactoring

--- public_html/php/oauth.php
+++ public_html/php/oauth.php
@@ -336,7 +336,7 @@ class MW_OAuth {
 
 		$ch = curl_init();
 		curl_setopt( $ch, CURLOPT_URL, $url );
-		curl_setopt( $ch, CURLOPT_HTTPHEADER, [ $header  );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, [ $header ] );
 		//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 		curl_setopt( $ch, CURLOPT_USERAGENT, $this->gUserAgent );
 		curl_setopt( $ch, CURLOPT_HEADER, 0 );
